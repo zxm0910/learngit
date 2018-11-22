ae0861d9dd29c6bcd735a040e7adea92428c04fc
Zhang Xiaomin <17916603@qq.com>
2018-11-22 13:35:58 +0800

    mysql save

--- www/orm.py
+++ www/orm.py
@@ -7,8 +7,10 @@ import asyncio, logging
 
 import aiomysql
 
+
 def log(sql, args=()):
     logging.info('SQL: %s' % sql)
+
 
 async def create_pool(loop, **kw):
     logging.info('create database connection pool...')
@@ -16,15 +18,16 @@ async def create_pool(loop, **kw):
     __pool = await aiomysql.create_pool(
         host=kw.get('host', 'localhost'),
         port=kw.get('port', 3306),
-        user=kw['root'],
-        password=kw['root'],
-        db=kw['db'],
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['database'],
         charset=kw.get('charset', 'utf8'),
         autocommit=kw.get('autocommit', True),
         maxsize=kw.get('maxsize', 10),
         minsize=kw.get('minsize', 1),
         loop=loop
     )
+
 
 async def select(sql, args, size=None):
     log(sql, args)
@@ -38,6 +41,7 @@ async def select(sql, args, size=None):
                 rs = await cur.fetchall()
         logging.info('rows returned: %s' % len(rs))
         return rs
+
 
 async def execute(sql, args, autocommit=True):
     log(sql)
@@ -56,11 +60,13 @@ async def execute(sql, args, autocommit=True):
             raise
         return affected
 
+
 def create_args_string(num):
     L = []
     for n in range(num):
         L.append('?')
     return ', '.join(L)
+
 
 class Field(object):
 
@@ -73,30 +79,36 @@ class Field(object):
     def __str__(self):
         return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
 
+
 class StringField(Field):
 
     def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
         super().__init__(name, ddl, primary_key, default)
+
 
 class BooleanField(Field):
 
     def __init__(self, name=None, default=False):
         super().__init__(name, 'boolean', False, default)
 
+
 class IntegerField(Field):
 
     def __init__(self, name=None, primary_key=False, default=0):
         super().__init__(name, 'bigint', primary_key, default)
+
 
 class FloatField(Field):
 
     def __init__(self, name=None, primary_key=False, default=0.0):
         super().__init__(name, 'real', primary_key, default)
 
+
 class TextField(Field):
 
     def __init__(self, name=None, default=None):
         super().__init__(name, 'text', False, default)
+
 
 class ModelMetaclass(type):
 
@@ -133,6 +145,7 @@ class ModelMetaclass(type):
         attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
         attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
         return type.__new__(cls, name, bases, attrs)
+
 
 class Model(dict, metaclass=ModelMetaclass):
 
